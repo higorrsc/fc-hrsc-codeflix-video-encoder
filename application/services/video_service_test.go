3cfe01574b4a64caa2dc202abbd657c4dab7aa51
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-05-17 19:59:14 -0300

    chore: add video service test

---
+++ application/services/video_service_test.go
@@ -0,0 +1,50 @@
+package services_test
+
+import (
+	"log"
+	"testing"
+	"time"
+
+	"github.com/higorrsc/fc-hrsc-codeflix-video-encoder/application/repositories"
+	"github.com/higorrsc/fc-hrsc-codeflix-video-encoder/application/services"
+	"github.com/higorrsc/fc-hrsc-codeflix-video-encoder/domain"
+	"github.com/higorrsc/fc-hrsc-codeflix-video-encoder/framework/database"
+	"github.com/joho/godotenv"
+	uuid "github.com/satori/go.uuid"
+	"github.com/stretchr/testify/require"
+)
+
+func init() {
+	err := godotenv.Load("../../.env")
+	if err != nil {
+		log.Fatalf("Error loading .env file")
+	}
+}
+
+func prepare() (*domain.Video, repositories.VideoRepositoryDb) {
+	db := database.NewDbTest()
+	defer db.Close()
+
+	video := domain.NewVideo()
+	video.ID = uuid.NewV4().String()
+	video.FilePath = "convite.mp4"
+	video.CreatedAt = time.Now()
+
+	repo := repositories.VideoRepositoryDb{Db: db}
+
+	return video, repo
+}
+
+func TestVideoServiceDownload(t *testing.T) {
+	video, repo := prepare()
+
+	videoService := services.NewVideoService()
+	videoService.Video = video
+	videoService.VideoRepository = repo
+
+	err := videoService.Download("codeflix")
+	require.Nil(t, err)
+
+	err = videoService.Fragment()
+	require.Nil(t, err)
+}
