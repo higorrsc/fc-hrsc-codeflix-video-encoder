3c9366e8b3f7ac0b8350815c39e28785d095fbd2
Higor Rodrigues dos Santos Cruz <higorrsc@gmail.com>
2025-05-21 21:22:15 -0300

    chore: add insert video method at VideoService

--- application/services/video_service.go
+++ application/services/video_service.go
@@ -124,6 +124,15 @@ func (v *VideoService) Finish() error {
 	return nil
 }
 
+func (v *VideoService) InsertVideo() error {
+	_, err := v.VideoRepository.Insert(v.Video)
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
 func printOutput(output []byte) {
 	if len(output) > 0 {
 		log.Printf("=====> Output: %s\n", string(output))
